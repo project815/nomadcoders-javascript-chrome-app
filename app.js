bd438fd90ead02841a747fdc3019d037a3d2201d
songminseok <ske3056@gmail.com>
2023-06-04 23:18:33 +0900

    variable

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("hello world");
+
+const a = 4;
+const b = 3;
+const myName = "Song";
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console.log("my name is " + myName);
+
+const c = null;
+console.log(c);
