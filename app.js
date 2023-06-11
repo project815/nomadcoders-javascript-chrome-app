09772390764df9d8c5135c5cec66aee40b33314c
songminseok <ske3056@gmail.com>
2023-06-12 00:30:25 +0900

    getElementById, querySelector

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const title = document.getElementById("title");
+const hellos = document.getElementsByClassName("hello");
 
-console.dir(title);
+console.log(hellos);
 
-title.innerText = "hello world";
+const tag = document.getElementsByTagName("h1");
 
-console.log(title.id);
-// console.log(title.)
-console.log(title.className);
+console.log(tag);
+
+const css = document.querySelectorAll(".hello h1:first-child");
+//Returns the first element that is a descendant of node that matches selectors.
+
+console.log(css);
+
+const ti2 = document.querySelector("#hello");
+//querySelector : class, id,를 둘다 가져옴..  // . 과 #으로 구분
+const ti1 = document.getElementById("hello");
+//getElementById : id로 명시
