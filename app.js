d692afb3020f554fb90a851fa87cfff9ff3d54c2
songminseok <ske3056@gmail.com>
2023-06-11 20:46:10 +0900

    function

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
-const playerName = "nico";
-const playerPoints = 121212;
-const playerHandsome = true;
-const playerFat = "little bit";
-
-console.log();
-
-//player[0] == name
-//p;ayer[1] == points
-//...
-const player = ["nico", 1212, false, "little bit"];
-
-//>> object
-const player1 = {
-  name: "nico",
-  point: 1212,
-  handsome: false,
-  playerFat: "little bit",
+const calculator = {
+  plus: function (a, b) {
+    alert(a + b);
+  },
+  minus: function (a, b) {
+    alert(a - b);
+  },
+  times: function (a, b) {
+    alert(a * b);
+  },
+  divide: function (a, b) {
+    alert(a / b);
+  },
+  power: function (a, b) {
+    alert(a ** b);
+  },
 };
 
-console.log(player1);
-console.log(player1.name);
-console.log(player1["name"]);
-
-player1.name = "song";
-
-console.log(player1.name);
-
-// player1 = "?"; constansts
-player1.lastName = "pototo";
-
-console.log(player1);
+calculator.plus(1, 2);
+console.log(calculator.plus(1, 2));
