e5f045f82f0a2e55c37768daac67f642752cdf96
songminseok <ske3056@gmail.com>
2023-06-12 00:48:20 +0900

    querySelectorAll

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ const tag = document.getElementsByTagName("h1");
 
 console.log(tag);
 
-const css = document.querySelectorAll(".hello h1:first-child");
+const title = document.querySelector(".hello h1");
 //Returns the first element that is a descendant of node that matches selectors.
 
-console.log(css);
+const test = document.querySelectorAll(".hello h1:first-child");
+//Returns all element descendants of node that match selectors.
+title.innerText = "hello";
+console.log(title);
 
+//change test.innerText to "hello" All
+test.innerText = "hello";
+console.log("test:", test);
 const ti2 = document.querySelector("#hello");
 //querySelector : class, id,를 둘다 가져옴..  // . 과 #으로 구분
 const ti1 = document.getElementById("hello");
