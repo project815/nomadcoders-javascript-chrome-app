03764396597b1f301fbcffae2296763dc8118dfe
songminseok <ske3056@gmail.com>
2023-06-12 00:59:10 +0900

    About Event

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-const hellos = document.getElementsByClassName("hello");
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(hellos);
+console.dir(title);
 
-const tag = document.getElementsByTagName("h1");
+title.style = "color:red";
 
-console.log(tag);
+const handleTitleClick = () => {
+  console.log("helloWorld");
+};
 
-const title = document.querySelector(".hello h1");
-//Returns the first element that is a descendant of node that matches selectors.
-
-const test = document.querySelectorAll(".hello h1:first-child");
-//Returns all element descendants of node that match selectors.
-title.innerText = "hello";
-console.log(title);
-
-//change test.innerText to "hello" All
-test.innerText = "hello";
-console.log("test:", test);
-const ti2 = document.querySelector("#hello");
-//querySelector : class, id,를 둘다 가져옴..  // . 과 #으로 구분
-const ti1 = document.getElementById("hello");
-//getElementById : id로 명시
+title.addEventListener("click", handleTitleClick);
