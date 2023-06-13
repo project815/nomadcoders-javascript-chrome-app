a780d9d433b9f092c2258ea3206e2ffad589e729
songminseok <ske3056@gmail.com>
2023-06-13 23:57:05 +0900

    change className, so css animation

--- app.js
+++ app.js
@@ -2,48 +2,26 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 console.dir(h1);
 
-h1.style = "color:red";
+// const handleTitleClick = () => {
+//   const currentColor = h1.style.color;
+//   let newColor;
+//   if (currentColor === "blue") {
+//     newColor = "tomato";
+//   } else {
+//     newColor = "blue";
+//   }
+//   h1.style.color = newColor;
 
-const handleTitleClick = () => {
-  console.log("helloWorld");
-  h1.style = "color:blue";
-};
+//   h1.style.color = h1.style.color === "blue" ? "tomato" : "blue";
+// };
 
-// h1.addEventListener("click", handleTitleClick);
-h1.onclick = handleTitleClick;
-
-// h1.addEventListener("mouseenter", () => {
-//   h1.innerText = "mouse is here";
-//   console.log("mouseEnter");
-// });
-h1.onmouseenter = () => {
-  h1.innerText = "mouse is here";
-};
-
-// h1.addEventListener("mouseleave", () => {
-//   h1.innerText = "mouse is out of here";
-//   console.log("mouseLeave");
-// });
-h1.onmouseleave = () => {
-  h1.innerText = "mouse is out of here";
-};
-
-//그치만 addEventListener를 사용하는 것이 더 좋다.
-//왜냐하면 removeEventListener를 사용할 수 있기 때문이다.
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
+function changeColor() {
+  const clickedCloss = "clicked";
+  console.log("click");
+  if (h1.className === clickedCloss) {
+    h1.className = "";
+  } else {
+    h1.className = clickedCloss; //상수가 정해짐. - 좋은 방법은 아님. 모든 className를 정리하지 않았으면 좋겠다는 생각을 한다.
+  }
 }
-window.addEventListener("resize", handleWindowResize);
-
-window.addEventListener("copy", () => {
-  alert("copy");
-});
-
-window.addEventListener("offline", () => {
-  alert("SOS no Wifi");
-});
-
-window.addEventListener("online", () => {
-  alert("all good");
-});
+h1.addEventListener("click", changeColor);
