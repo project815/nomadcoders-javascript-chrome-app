910c292dee0e53385f298e80d9088f589d6a2a4a
songminseok <ske3056@gmail.com>
2023-06-13 23:07:36 +0900

    more event

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(title);
+console.dir(h1);
 
-title.style = "color:red";
+h1.style = "color:red";
 
 const handleTitleClick = () => {
   console.log("helloWorld");
-  title.style = "color:blue";
+  h1.style = "color:blue";
 };
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", () => {
-  title.innerText = "mouse is here";
-  console.log("mouseEnter");
+// h1.addEventListener("click", handleTitleClick);
+h1.onclick = handleTitleClick;
+
+// h1.addEventListener("mouseenter", () => {
+//   h1.innerText = "mouse is here";
+//   console.log("mouseEnter");
+// });
+h1.onmouseenter = () => {
+  h1.innerText = "mouse is here";
+};
+
+// h1.addEventListener("mouseleave", () => {
+//   h1.innerText = "mouse is out of here";
+//   console.log("mouseLeave");
+// });
+h1.onmouseleave = () => {
+  h1.innerText = "mouse is out of here";
+};
+
+//그치만 addEventListener를 사용하는 것이 더 좋다.
+//왜냐하면 removeEventListener를 사용할 수 있기 때문이다.
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+window.addEventListener("resize", handleWindowResize);
+
+window.addEventListener("copy", () => {
+  alert("copy");
 });
-title.addEventListener("mouseleave", () => {
-  title.innerText = "mouse is out of here";
-  console.log("mouseLeave");
+
+window.addEventListener("offline", () => {
+  alert("SOS no Wifi");
+});
+
+window.addEventListener("online", () => {
+  alert("all good");
 });
