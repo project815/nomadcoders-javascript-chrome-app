cd7541e1a81a6311351a0f4a685e40b09936459a
songminseok <ske3056@gmail.com>
2023-06-18 18:38:09 +0900

    login Input form

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
+// == const loginForm = document.getElementById("loginForm");
+
+const loginInput = loginForm.querySelector("input");
+// == const InputInput = document.querySelector("#loginForm input");
+const loginButton = loginForm.querySelector("button");
+// == const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
+  console.dir(loginInput);
+  console.log("Clcik");
+  console.log("clcike : ", loginInput.value);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
