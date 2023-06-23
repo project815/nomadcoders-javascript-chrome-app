355b686fb6e0ef5e3f421b5e1d1412928636b366
songminseok <ske3056@gmail.com>
2023-06-23 23:35:38 +0900

    review event

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
 const loginForm = document.querySelector("#login-form");
-// == const loginForm = document.getElementById("loginForm");
+const loginInput = document.querySelector("#login-form input");
 
-const loginInput = loginForm.querySelector("input");
-// == const InputInput = document.querySelector("#loginForm input");
-const loginButton = loginForm.querySelector("button");
-// == const loginButton = document.querySelector("#login-form button");
-
-function onLoginBtnClick(info) {
-  info.preventDefault();
-  console.log(info);
-  // console.dir(loginInput);
-  // console.log("Clcik");
-  // console.log("clcike : ",/ loginInput.value);
-
-  // check user  >> css requried maxlength
-  // const username = loginInput.value;
-  // if (username === "") {
-  //   alert("Please write your name");
-  // } else if (username.length > 15) alert("You name is too long");
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  console.log(event);
 }
 
-// loginButton.addEventListener("click", onLoginBtnClick);
-
-loginForm.addEventListener("submit", onLoginBtnClick);
-
-onLoginBtnClick();
+loginForm.addEventListener("submit", onLoginSubmit);
