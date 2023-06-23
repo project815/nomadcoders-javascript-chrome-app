91f6ef83cfe5e365c5083198e11a1461984f553b
songminseok <ske3056@gmail.com>
2023-06-24 00:00:43 +0900

    오타 조심!

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
-  console.log(event);
+  console.log(username);
 }
 
+function handleLinkClick() {
+  alert("Clicked");
+}
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+
+// handleLinkClick({information about the event that just happened})
