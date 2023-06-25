8ef3556d270bc7c0d5540c943de0c0766e125168
songminseok <ske3056@gmail.com>
2023-06-25 22:47:33 +0900

    getting username

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
 const link = document.querySelector("a");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   console.log(username);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerHTML = `hello ${username}`;
 }
 
-function handleLinkClick() {
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.log(event);
   alert("Clicked");
 }
 loginForm.addEventListener("submit", onLoginSubmit);
