0d9f7d1d1053da0710872ef18c0383c64472ba7f
songminseok <ske3056@gmail.com>
2023-06-28 23:59:20 +0900

    localstorage review

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
   console.log(username);
   greeting.classList.remove(HIDDEN_CLASSNAME);
@@ -24,5 +26,15 @@ loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLinkClick);
 
 // handleLinkClick({information about the event that just happened})
-localStorage.setItem("username", "nico");
 localStorage.getItem("username");
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  //show
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerHTML = `hello ${savedUsername}`;
+}
