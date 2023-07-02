4c59f3806537116b49c1f0714a9199826b79cdb4
songminseok <ske3056@gmail.com>
2023-07-02 23:15:22 +0900

    PadStart

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,11 @@
-const clock = document.querySelectorAll("h2#clock");
+const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getClock() {
+  const date = new Date();
+  console.log(
+    `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`
+  );
+  clock.innerText = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
 }
-
-setInterval(sayHello, 1000);
+getClock();
+setInterval(getClock, 1000);
